bc03a3a9d64413f38ed7de1e0d3ac6eff1e855d6
TeddyFrye <tfrye1227@gmail.com>
2023-03-27 10:26:41 -0500

    Remove all instances of map, begin tictactoe vers

--- script.js
+++ script.js
@@ -1,58 +1,46 @@
-/*
-** The Gameboard represents the state of the board
-** Each equare holds a Cell (defined later)
-** and we expose a dropToken method to be able to add Cells to squares
-*/
-
 function Gameboard() {
-    const rows = 6;
-    const columns = 7;
+    const rows = 2;
+    const columns = 2;
     const board = [];
-  
-    // Create a 2d array that will represent the state of the game board
-    // For this 2d array, row 0 will represent the top row and
-    // column 0 will represent the left-most column.
-    // This nested-loop technique is a simple and common way to create a 2d array.
+
     for (let i = 0; i < rows; i++) {
       board[i] = [];
       for (let j = 0; j < columns; j++) {
         board[i].push(Cell());
       }
     }
-  
-    // This will be the method of getting the entire board that our
-    // UI will eventually need to render it.
+
     const getBoard = () => board;
-  
-    // In order to drop a token, we need to find what the lowest point of the
-    // selected column is, *then* change that cell's value to the player number
+
     const dropToken = (column, player) => {
-      // Our board's outermost array represents the row,
-      // so we need to loop through the rows, starting at row 0,
-      // find all the rows that don't have a token, then take the
-      // last one, which will represent the bottom-most empty cell
-      const availableCells = board.filter((row) => row[column].getValue() === 0).map(row => row[column]);
-  
-      // If no cells make it through the filter, 
-      // the move is invalid. Stop execution.
+      const availableCells = [];
+      for (let i = rows - 1; i >= 0; i--) {
+        if (board[i][column].getValue() === 0) {
+          availableCells.push(board[i][column]);
+          break;
+        }
+      }
+
       if (!availableCells.length) return;
-  
-      // Otherwise, I have a valid cell, the last one in the filtered array
-      const lowestRow = availableCells.length - 1;
-      board[lowestRow][column].addToken(player);
+
+      availableCells[0].addToken(player);
     };
 
-  // This method will be used to print our board to the console.
-  // It is helpful to see what the board looks like after each turn as we play,
-  // but we won't need it after we build our UI
-  const printBoard = () => {
-    const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
-    console.log(boardWithCellValues);
-  };
+    const printBoard = () => {
+      const boardWithCellValues = [];
+      for (let i = 0; i < rows; i++) {
+        const rowWithCellValues = [];
+        for (let j = 0; j < columns; j++) {
+          rowWithCellValues.push(board[i][j].getValue());
+        }
+        boardWithCellValues.push(rowWithCellValues);
+      }
+      console.log(boardWithCellValues);
+    };
 
-  // Here, we provide an interface for the rest of our
-  // application to interact with the board
-  return { getBoard, dropToken, printBoard };
+    return { getBoard, dropToken, printBoard };
+}
+
 }
 
 /*
