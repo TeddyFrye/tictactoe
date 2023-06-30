12e1ba6dd15581efaf7f930a72f495d1c6410160
TeddyFrye <tfrye1227@gmail.com>
2023-06-30 11:20:13 -0500

    Minor styling changes

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
 function gameBoard() {
-    const rows = 3;
-    const columns = 3;
-    const cells = [];
-//For loop creating empty cells
-    for (let i = 0; i < rows; i++) {
-        cells[i] = [];
-      for (let j = 0; j < columns; j++) {
-        cells[i].push(Cell());
-      }
+  const rows = 3;
+  const columns = 3;
+  const cells = [];
+  //For loop creating empty cells
+  for (let i = 0; i < rows; i++) {
+    cells[i] = [];
+    for (let j = 0; j < columns; j++) {
+      cells[i].push(Cell());
     }
+  }
 
-    const getBoard = () => cells;
+  const getBoard = () => cells;
 
-    
-    const placeToken = (row, column, player) => {
-        // get cell at row and column
-        const cell = cells[row][column]
+  const placeToken = (row, column, player) => {
+    // get cell at row and column
+    const cell = cells[row][column];
 
-        // set cell to player token
-        cell.toPlayer(player)
-    };
-    // Nothing preventing placing token over other player
+    // set cell to player token
+    cell.toPlayer(player);
+  };
+  // Nothing preventing placing token over other player
 
-    const printBoard = () => {
-      const boardWithCellValues = [];
-      for (let i = 0; i < rows; i++) {
-        const rowWithCellValues = [];
-        for (let j = 0; j < columns; j++) {
-          rowWithCellValues.push(cells[i][j].getValue());
-        }
-        boardWithCellValues.push(rowWithCellValues);
+  const printBoard = () => {
+    const boardWithCellValues = [];
+    for (let i = 0; i < rows; i++) {
+      const rowWithCellValues = [];
+      for (let j = 0; j < columns; j++) {
+        rowWithCellValues.push(cells[i][j].getValue());
       }
-      console.log(boardWithCellValues);
-    };
-return { getBoard,
-    placeToken,
-    printBoard };
+      boardWithCellValues.push(rowWithCellValues);
+    }
+    console.log(boardWithCellValues);
+  };
+  return { getBoard, placeToken, printBoard };
 }
-
 
 function Cell() {
   let value = 0;
@@ -45,18 +41,18 @@ function Cell() {
   const setValue = (to_value) => {
     value = to_value;
   };
-  
+
   const getValue = () => value;
 
   const toPlayer = (player) => {
-    setValue(player.token)
-  }
+    setValue(player.token);
+  };
 
   return {
     setValue,
     getValue,
     toPlayer,
-    value
+    value,
   };
 }
 
@@ -69,12 +65,12 @@ function gameController(
   const players = [
     {
       name: playerOneName,
-      token: 1
+      token: 1,
     },
     {
       name: playerTwoName,
-      token: 2
-    }
+      token: 2,
+    },
   ];
 
   let activePlayer = players[0];
@@ -88,10 +84,12 @@ function gameController(
     board.printBoard();
     console.log(`${getActivePlayer().name}'s turn.`);
   };
-//Here is what you use to play a round, e.g. "game.playRound(0,1)"
+  //Here is what you use to play a round, e.g. "game.playRound(0,1)"
   const playRound = (row, column) => {
     console.log(
-      `Placing ${getActivePlayer().name}'s token into row ${row}, column ${column}...`
+      `Placing ${
+        getActivePlayer().name
+      }'s token into row ${row}, column ${column}...`
     );
     board.placeToken(row, column, getActivePlayer());
 
@@ -100,9 +98,9 @@ function gameController(
   };
 
   printNewRound();
-    return {
+  return {
     playRound,
-    getActivePlayer
+    getActivePlayer,
   };
 }
 
